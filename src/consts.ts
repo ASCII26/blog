bbc1fcb6628b0dd7886f6f2272c1e6288a3c60be
ASCII26 <422920870@qq.com>
2025-05-19 17:46:28 +0800

    feat: init with astro

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "沐洒布吉岛";
+export const SITE_DESCRIPTION = "Welcome to my website!";
